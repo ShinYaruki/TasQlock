5ab39c15491cc57920f9687c29987b285a9641aa
ShinYaruki <5.1.1.cn7568m325373c@gmail.com>
2020-04-03 22:19:36 +0900

    task_controller_minor_fixed
    ・taskコントローラ内の記述に関し、ストロングパラメータを使用して重複部分を簡略化
    ・軽微な変数名の修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
   before_action :login_required
+  before_action :schedule_list_get, only: [:new, :edit]
   before_action :set_task, only:[:edit, :update, :destroy, :done, :calendardone]
+  before_action :set_today, only:[:new, :today]
 
   def new
     @task = Task.new
-    @schedule_list = Task.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(starttime: "asc")
-    @today = Time.zone.now.strftime("%Y%m%d")
   end
 
   def create
@@ -13,10 +13,6 @@ class TasksController < ApplicationController
       if @task.save
         flash[:notice] = "スケジュールを登録しました"
         redirect_to new_task_path
-        # respond_to do |format| 非同期通信でcreateを発火予定だったが保留
-        #   format.html {redirect_to new_task_path}
-        #   format.json
-        # end 
       else
         flash[:alert] = @task.errors.full_messages
         redirect_to new_task_path
@@ -26,50 +22,43 @@ class TasksController < ApplicationController
   def edit
     @starttime = @task.starttime.strftime("%H:%M")
     @finishtime = @task.finishtime.strftime("%H:%M")
-    @schedule_list = Task.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(starttime: "asc")
   end
 
   def update
-      if @task.update(schedule_params)
-        redirect_to today_tasks_path
-        flash[:notice] = "スケジュールを編集しました"
-      else
-        flash[:alert] = @task.errors.full_messages
-        redirect_to edit_task_path
-      end
+    if @task.update(schedule_params)
+      redirect_to today_tasks_path
+      flash[:notice] = "スケジュールを編集しました"
+    else
+      flash[:alert] = @task.errors.full_messages
+      redirect_to edit_task_path
+    end
   end
 
   def destroy
-    task.destroy
+    @task.destroy
     flash[:notice] = "スケジュールを削除しました"
     redirect_to today_tasks_path
   end
 
   def today
-    date = Time.zone.now.to_s.slice(0..9)
-    @month = date.month
-    @day = date.day
+    @month = @date.month
+    @day = @date.day
     @tasks = Task.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(starttime: "asc")
     @memo = Memo.new
     @memos = Memo.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(created_at: "asc")
   end
 
   def calendar
-    @tmp = params[:text] #日付情報を受取る為、_calendar_bar.html.hamlに架空inputを設置しており、application.js経由でparams[:text]を取得している
-    date = @tmp.to_date
-    @month = date.month
-    @day = date.day
-    @cale = Task.where(date: @tmp).where(user_id: current_user.id).order(starttime: "asc")
+    @tmp = params[:text].to_date #日付情報を受取る為、_calendar_bar.html.hamlに架空inputを設置しており、application.js経由でparams[:text]を取得している
+    @month = @tmp.month
+    @day = @tmp.day
+    @calendar = Task.where(date: @tmp).where(user_id: current_user.id).order(starttime: "asc")
     @memos = Memo.where(date: @tmp).where(user_id: current_user.id).order(created_at: "asc")
   end
 
   def done
     @task.update(done: true)
     redirect_to today_tasks_path
-    # respond_to do |format| 非同期通信でdoneを発火予定だったが保留
-    #   format.html {redirect_to today_tasks_path}
-    #   format.json
-    # end 
     flash[:notice] = "スケジュールが完了しました"
   end
 
@@ -82,6 +71,14 @@ class TasksController < ApplicationController
   private
     def set_task
       @task = Task.find(params[:id])
+    end
+
+    def set_today
+      @date = Time.zone.now.strftime("%Y-%m-%d").to_date
+    end
+
+    def schedule_list_get
+      @schedule_list = Task.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(starttime: "asc")
     end
 
     def schedule_params
--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,15 @@ class Task < ApplicationRecord
   validates :finishtime, presence: { message: 'を00:00〜24:00の値で入力して下さい' }
   validates :title, presence: true
   validates :user_id, presence: true
+
+  # def self.current_user
+  #   User.find_by(id: session[:user_id]) if session[:user_id]
+  # end
+
+  # def self.newtask
+  #   {
+  #     schedule_list: Task.where(date: Time.zone.now.beginning_of_day).where(user_id: @current_user.id).order(starttime: "asc"),
+  #     today: Time.zone.now.strftime("%Y%m%d")
+  #   }
+  # end
 end
