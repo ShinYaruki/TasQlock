44ca99e2d7653f10a3f93c8d5f52201e97657dfd
ShinYaruki <5.1.1.cn7568m325373c@gmail.com>
2020-04-03 21:05:55 +0900

    task_controller_edited
    ・@task = Task.find(params[:id])をストロングパラメータのset_taskというメソッドに書き、必要なアクションにはbefore_actionで呼び出すよう変更。
    ・日付の値をdate.monthのように変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :login_required
+  before_action :set_task, only:[:edit, :update, :destroy, :done, :calendardone]
 
   def new
     @task = Task.new
@@ -23,14 +24,12 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
     @starttime = @task.starttime.strftime("%H:%M")
     @finishtime = @task.finishtime.strftime("%H:%M")
     @schedule_list = Task.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(starttime: "asc")
   end
 
   def update
-    @task = Task.find(params[:id])
       if @task.update(schedule_params)
         redirect_to today_tasks_path
         flash[:notice] = "スケジュールを編集しました"
@@ -41,7 +40,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = Task.find(params[:id])
     task.destroy
     flash[:notice] = "スケジュールを削除しました"
     redirect_to today_tasks_path
@@ -49,23 +47,23 @@ class TasksController < ApplicationController
 
   def today
     date = Time.zone.now.to_s.slice(0..9)
-    @month = date.slice(5..6)
-    @day = date.slice(8..9)
+    @month = date.month
+    @day = date.day
     @tasks = Task.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(starttime: "asc")
     @memo = Memo.new
     @memos = Memo.where(date: Time.zone.now.beginning_of_day).where(user_id: current_user.id).order(created_at: "asc")
   end
 
   def calendar
-    @tmp = params[:text]
-    @month = @tmp[5..6]
-    @day = @tmp[8..9]
+    @tmp = params[:text] #日付情報を受取る為、_calendar_bar.html.hamlに架空inputを設置しており、application.js経由でparams[:text]を取得している
+    date = @tmp.to_date
+    @month = date.month
+    @day = date.day
     @cale = Task.where(date: @tmp).where(user_id: current_user.id).order(starttime: "asc")
     @memos = Memo.where(date: @tmp).where(user_id: current_user.id).order(created_at: "asc")
   end
 
   def done
-    @task = Task.find(params[:id])
     @task.update(done: true)
     redirect_to today_tasks_path
     # respond_to do |format| 非同期通信でdoneを発火予定だったが保留
@@ -76,13 +74,16 @@ class TasksController < ApplicationController
   end
 
   def calendardone
-    @task = Task.find(params[:id])
     @task.update(done: true)
     redirect_to today_tasks_path
     flash[:notice] = "#{@task.date}のスケジュールが完了しました"
   end
 
   private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
     def schedule_params
       params.require(:task).permit(:date, :starttime, :finishtime, :done, :tag_id, :title, :description).merge(user_id: current_user.id)
     end
